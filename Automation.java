6d0795d37ba1442f97b3c4dfe5a3188454a9c9d2
nandy0311 <107383470+nandy0311@users.noreply.github.com>
2022-06-30 18:30:27 +0530

    Update Automation.java

--- Automation.java
+++ Automation.java
@@ -1,11 +1,12 @@
 package week3.day1;
 
-public class Automation implements Language,TestTool {
+public class Automation extends MultipleLanguage implements Language,TestTool {
 
 	public static void main(String[] args) {
 		Automation obj= new Automation();
 		obj.Java();
 		obj.Selenium();
+		obj.ruby();
 	//	Language obj1= new Automation();
 	
 	}
@@ -19,6 +20,12 @@ public class Automation implements Language,TestTool {
 	@Override
 	public void Selenium() {
 		System.out.println("Selenium is a tool-overriden from testing tool interface");
+	}
+
+	@Override
+	public void python() {
+		System.out.println("Python is a language -overriden from Multiple Language parent class");
+		
 	}
 
 }
